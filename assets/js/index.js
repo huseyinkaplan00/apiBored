85aef9d977a7394f135ba6041be35a8905c6dc49
huseyinkaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-02-19 17:28:12 +0300

    Bored App

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("click", (e) => {
+  if (e.target.id === "main-buttton") {
+    mainButton();
+  }
+});
+
+const mainButton = () => {
+  document.querySelector(
+    ".video"
+  ).innerHTML = `<video autoplay muted loop class="bgVideo">
+    <source src="./assets/img/bgg.mp4" type="video/mp4" />
+    Your browser does not support HTML5 video.
+  </video>`;
+  container();
+};
+
+const container = () => {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector(".main-container").innerHTML = `
+        <div class="tip-container">
+        <div class="tip-box">
+          <p>
+           ${data.activity}
+          </p>
+        </div>
+      </div>
+      <button id="buttonTwo">Get New Advice</button>
+        `;
+      document
+        .getElementById("buttonTwo")
+        .addEventListener("click", () => container());
+    });
+};
